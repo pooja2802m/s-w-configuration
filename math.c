8fef7fea09b04a484a78510d297823f909c12720
pooja2802m <pooja2802b@gmail.com>
2015-10-13 23:14:29 +0530

    enter values
    enter the value of a and b

---
+++ math.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,c;
+clrscr();
+printf("Enter value of a: ");
+scanf("%d",&a);
+printf("Enter value of b: ");
+scanf("%d",&b);
+getch();
+}
